5b05538e2d102e00949edc667b85df102f770186
dandel <dandel>
2019-02-22 22:42:44 -0500

    feature(oauth2): add api oauth middleware

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,19 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('foo', function () {
+    return response('Hello World');
+});
+
+Route::prefix('')->middleware('auth:api')->group(function () {
+    Route::get('properties', function () {
+        return response()->json([
+            'APP_NAME' => env('APP_NAME'),
+            'APP_ENV' => env('APP_ENV'),
+            'APP_KEY' => env('APP_KEY'),
+            'APP_DEBUG' => env('APP_DEBUG'),
+            'APP_URL' => env('APP_URL')
+        ]);
+    });
+});
